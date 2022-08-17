bf5cef8a83e1a44fc304be19fc47e635ab5c5bfc
RamiBelhadj <belhadjmohamedrami@gmail.com>
2022-08-17 22:51:48 +0200

    Add banking menu

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'okok#3488'
+description 'okokBanking'
+
+ui_page 'web/ui.html'
+
+files {
+	'web/*.*'
+}
+
+shared_script {
+	'@qb-core/import.lua',
+	'config.lua'
+}
+
+
+client_scripts {
+	'client.lua'
+
+}
+
+server_scripts {
+	'server.lua'
+}
